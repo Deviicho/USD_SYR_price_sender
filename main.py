4911bae5685b714dbe2033d1a6383f4a6b4b782d
Deviicho <devviicho@gmail.com>
2025-06-23 14:19:29 +0300

    first one

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
+from decouple import config
+
+
+url = 'https://sp-today.com/en'
+page = requests.get(url)
+
+
+def main(page):
+    src = page.content
+    soup = BeautifulSoup(src , 'lxml')
+    
+    header = soup.find('header' , {'class' : 'navbar-fixed-top'})
+    a = header.find('a' , {'href' : 'https://sp-today.com/en/currency/us_dollar/city/damascus'})
+    USD_price = a.find('span' , {'class' : 'value'})
+    SYRtoUSD =USD_price.text.strip()
+    
+    header = soup.find('header' , {'class' : 'navbar-fixed-top'})
+    a = header.find('a' , {'href' : 'https://sp-today.com/en/gold/karat21'})
+    GOLD_price = a.find('span' , {'class' : 'value'})
+    GOLD_GRAM = GOLD_price.text.strip()
+    
+    sender = config('SENDER')
+    app_password = config('APP_PASSWORD')
+    receiver = ['michoar777@gmail.com'] #use ', '
+
+    msg = MIMEText(f"Good morning Mate!\n\n\nhere is some news for today:\n\nthe price for usd is : {SYRtoUSD}\nand the price for gold is : {GOLD_GRAM}")
+    msg['Subject'] = 'Daily prices'
+    msg['From'] = sender
+    msg['To'] = ", ".join(receiver)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(sender, app_password)
+        smtp.send_message(msg)
+    
+    
+main(page)
+
+
+
